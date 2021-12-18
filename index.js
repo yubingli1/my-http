3189e260e77f65345bfc4ebe514af80e61a54aa7
yu <2602860836@qq.com>
2021-12-18 17:26:53 +0800

    fix chunxked

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'application/json'
-    });
-    
-    res.end(JSON.stringify({
+    const str = JSON.stringify({
         data: 'Hello World!'
-    }));
+    });
+
+    res.writeHead(200, {
+        'Content-Type': 'application/json',
+        'Content-Length': str.length
+    });
+
+    res.end(str);
 });
 
 server.listen(8000);
