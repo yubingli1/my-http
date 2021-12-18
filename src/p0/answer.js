95d394055414ef8d61e28ec7dc692d7992e4a419
yu <2602860836@qq.com>
2021-12-18 19:25:07 +0800

    fix month

---
+++ src/p0/answer.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const dateNow = new Date();
+const month_en = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+const server = http.createServer((req, res) => {
+    const str = JSON.stringify({
+        data: 'Hello World!',
+        year: dateNow.getFullYear(),
+        month: month_en[dateNow.getMonth()],
+        date: dateNow.getDate(),
+    },
+        space = '\t',
+        space = 2
+    );
+
+    res.writeHead(200, {
+        'Content-Type': 'application/json',
+        'Content-Length': str.length
+    });
+
+    res.end(str);
+});
+
+server.listen(8000);
