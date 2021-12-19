1bb9ace27e543a5a8cfa20fba463f164aa2bb2af
yu <2602860836@qq.com>
2021-12-19 20:35:10 +0800

    p1's answer

---
+++ src/p1/answer.js
@@ -0,0 +1,26 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    //console.log(req);
+
+    if (req.url == '/ok') {
+        const str = 'ok';
+
+        res.writeHead(200, {
+            'Content-Type': 'application/json',
+            'Content-Length': str.length
+        });
+
+        res.end(str);
+    }
+
+    else {
+        res.writeHead(404, {
+            'Content-Type': 'application/json'
+        });
+
+        res.end();
+    }
+});
+
+server.listen(8000);
