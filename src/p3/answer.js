566df724e32aa3484d8ae8842b3515c4d3548903
yu <2602860836@qq.com>
2021-12-25 21:36:22 +0800

    Removed a pair of parentheses

--- src/p3/answer.js
+++ src/p3/answer.js
@@ -20,7 +20,7 @@ function route(url) {
 const server = http.createServer((req, res) => {
     //const myURL = new URL('https://localhost:8000/phone?kw=a');
     const myURL = new URL(req.url, `http://${req.headers.host}`);
-    const keyword = (myURL.searchParams.get('kw'));
+    const keyword = myURL.searchParams.get('kw');
     const result = route(req.url)()?.filter(arr => arr.includes(keyword));
 
     res.writeHead(200, {
