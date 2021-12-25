30e37b6452b1f8be8449a875e5739047d68edd5b
yu <2602860836@qq.com>
2021-12-26 00:29:18 +0800

    Troublesome version

--- src/p3/answer.js
+++ src/p3/answer.js
@@ -17,19 +17,26 @@ function getEmpty() {
     return null;
 }
 
-function getFilteredPhoneList(keyword) {
-    const value = database.phone;
-    return value.filter(arr => arr.includes(keyword));
+function getFilteredPhoneList(indexUrl, keyword) {
+    const value = routingtable[indexUrl];
+    return value().filter(arr => arr.includes(keyword));
+}
+
+function Filter(url){
+    if(url.includes('?kw=')) {
+        return getFilteredPhoneList;
+    } else {
+        return routingtable[url];
+    }
 }
 
 function route(url) {
-    if(url.includes('?kw=')) {
-        return getFilteredPhoneList;
-    } else if(url in routingtable) {
-        return routingtable[url];
-    } else {
-        return getEmpty;
+    for(let key in routingtable){
+        if(url.includes(key)) {
+            return Filter;
+        }
     }
+    return getEmpty;
 }
 
 const server = http.createServer();
@@ -38,7 +45,9 @@ server.on('request', function(req, res) {
     if(req.url !== '/favicon.ico') {
         const myURL = new URL(req.url, `http://${req.headers.host}`);
         const keyword = myURL.searchParams.get('kw');
-        const result = route(req.url)(keyword);
+        const indexLength = req.url.length - myURL.search.length;
+        const indexUrl = req.url.slice(0, indexLength);
+        const result = route(req.url)(req.url)(indexUrl, keyword);
 
         res.writeHead(200, {
             "Content-Type": 'application/json'
